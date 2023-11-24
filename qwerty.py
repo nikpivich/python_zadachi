b72ff118315463318a7720886069dc703fc76c5e
Nikita_Pivovarchik <pivichnikita@gmail.com>
2023-11-25 00:22:03 +0300

    aas

---
+++ qwerty.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+import time
+# Создаем окно
+window = tk.Tk()
+
+# Создаем виджет (надпись) с текстом 'Hello World'
+label = tk.Label(window, text='Hello World')
+
+# Размещаем виджет в окне
+
+
+
+
+
+window.mainloop()
